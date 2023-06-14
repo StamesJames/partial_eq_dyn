4ac4f29b4b133ebfd9d0391f42e9b4d8c053a413
Benny Smit <benedict.smit@udo.edu>
2023-06-14 21:26:14 +0200

    wrote some doc and doctests for AsAny and DynPartialEq

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,47 @@
 use std::any::Any;
 
+/// Use this to implement the functionality to cast any type zu a &dyn Any.
+/// Implement simply with:
+/// ```
+/// # use std::any::Any;
+/// # use dyn_partial_eq::AsAny;
+/// struct Test;
+/// impl AsAny for Test{
+///     fn as_any(&self) -> &dyn Any {
+///         self
+///     }
+/// }
+/// let test_any: &dyn Any = Test.as_any();
+/// ```
 pub trait AsAny: Any {
     fn as_any(&self) -> &dyn Any;
 }
+
+/// Use this as supertrait of a Trait you want to compare as dyn object.
+/// ```
+/// # use std::any::Any;
+/// # use dyn_partial_eq::{DynPartialEq, AsAny};
+///
+/// trait TestTrait: DynPartialEq + AsAny{}
+/// #[derive(PartialEq)]
+/// struct Test;
+/// impl TestTrait for Test{}
+/// # impl AsAny for Test{
+/// #    fn as_any(&self) -> &dyn Any {
+/// #        self
+/// #    }
+/// # }
+/// impl DynPartialEq for Test{
+///     fn dyn_eq(&self, other: &dyn Any) -> bool {
+///         other
+///             .downcast_ref::<Test>()
+///             .map_or(false, |other| self == other)
+///     }
+/// }
+/// let test_dyn1: &dyn TestTrait = &Test;
+/// let test_dyn2: &dyn TestTrait = &Test;
+/// assert!(test_dyn1.dyn_eq(test_dyn2.as_any()));
+/// ```
 pub trait DynPartialEq {
     fn dyn_eq(&self, other: &dyn Any) -> bool;
 }
